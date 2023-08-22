850b88fd8f93979784322cf404f7dce7a3209c23
hinali <hinali030@gmail.com>
2023-08-22 06:31:06 -0600

    check the two argument same or not and give us message accordingly

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Hii", "Hii");
+assertEqual(1, 3);
+assertEqual(1, 1);
