1d527856bd8f909c4ae3cb6fd0e27f61e0ed640f
hinali <hinali030@gmail.com>
2023-08-22 07:58:24 -0600

    lint errors solved

--- head.js
+++ head.js
@@ -10,9 +10,9 @@ const assertEqual = function(actual, expected) {
 
 const head = function(headElement) {
   return headElement[0];
-  }
+};
 
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
 assertEqual(head([]), undefined);
