3a97bcb8e1dea169d569d4013612733defb78254
hinali <hinali030@gmail.com>
2023-08-27 16:29:09 -0600

    Implemented letterposition

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i <= sentence.length; i++) {
+
+    if (sentence[i] === " ") {
+      continue;
+    }
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [i];
+    } else {
+      results[sentence[i]].push(i);
+    }
+  }
+  console.log(results);
+
+  return results;
+};
+assertArraysEqual(letterPositions("hello").e, [1]);
