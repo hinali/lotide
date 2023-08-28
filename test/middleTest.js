8607883a5bbaee0a2242b39925db780de18b832a
hinali <hinali030@gmail.com>
2023-08-28 17:16:58 -0600

    refactored middleTest.js and tailTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle")
 
-assertArraysEqual(middle([1,2,3 ,4]), [2,3]);
-assertArraysEqual(middle([1,2,3]), [2]);
+describe("#middle",() => {
+
+  it("return [] for ['1', '2']",() => {
+  assert.deepEqual(middle([1, 2]), []);
+  });
+  it("return [3] for [1, 2, 3, 4, 5]",() => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("return [2, 3] for [1, 2, 3, 4]",() => {
+  assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  
+});
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe("#tail", () => {
+  it("check length is 2 for given array", () =>{
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
 
-const result1 = tail([1,2,3,4,5]);
-assertEqual(result1.length, 4);
-assertEqual(result1[0], 2);
-assertEqual(result1[1], 3);
-assertEqual(result1[2], 4);
-assertEqual(result1[3], 5);
+  it("check first element is 'Lighthouse' in resulting array'", () =>{
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+});
+
+
 
